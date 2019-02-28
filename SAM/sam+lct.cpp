605fefd033445e72accdb8260b6dc48a3bbad631
qiyue7 <zhangboqi007@live.com>
2019-02-28 16:58:50 +0800

    0

--- SAM/sam+lct.cpp
+++ SAM/sam+lct.cpp
@@ -1,3 +1,4 @@
+
 #include "pch.h"
 #include<string>
 #include<cstring>
@@ -18,94 +19,86 @@
 #include<functional>
 #define For(n) for(int i=0;i<n;++i)
 using namespace std;
-const int N = 200100;
-const int maxn = 400000;
+
 namespace sam_lct
 {
-	struct LCT {
-		int son[maxn][2], size[maxn], f[maxn], tag[maxn];
-#define root(x) (son[f[x]][0]!=x&&son[f[x]][1]!=x)
-
-		void push_down(int x) {
-			if (tag[x] == 0)return;
-			size[son[x][0]] += tag[x];
-			tag[son[x][0]] += tag[x];
-			size[son[x][1]] += tag[x];
-			tag[son[x][1]] += tag[x];
-			tag[x] = 0;
+	const int N = 1200100;
+	const int maxn = 2400000;
+	struct lct
+	{
+		int top;
+		int fa[N], c[N][2], w[N], tag[N], q[N];
+		bool rev[N];
+		void add(int x, int y) {
+			if (x)
+			{
+				w[x] += y; tag[x] += y;
+			}
 		}
-		void push_up(int x) {
-			if (!root(x))push_up(f[x]);
-			push_down(x);
+		void pushdown(int x) {
+			int l = c[x][0], r = c[x][1];
+			if (tag[x])
+			{
+				add(l, tag[x]); add(r, tag[x]);
+				tag[x] = 0;
+			}
 		}
-		void rt(int x, int k) {
-			int y = f[x], z = f[y];
-			son[y][k ^ 1] = son[x][k];
-			f[son[x][k]] = y;
-			son[x][k] = y;
-			f[y] = x; f[x] = z;
-			if (son[z][0] == y)son[z][0] = x;
-			else if (son[z][1] == y)son[z][1] = x;
-			push_down(y);
-			push_down(x);
+		bool isroot(int x) {
+			return c[fa[x]][0] != x && c[fa[x]][1] != x;
+		}
+		void rotate(int x) {
+			int y = fa[x], z = fa[y], l, r;
+			if (c[y][0] == x)l = 0; else l = 1; r = l ^ 1;
+			if (!isroot(y))
+			{
+				if (c[z][0] == y)c[z][0] = x;
+				else c[z][1] = x;
+			}
+			fa[c[x][r]] = y; fa[y] = x; fa[x] = z;
+			c[y][l] = c[x][r]; c[x][r] = y;
 		}
 		void splay(int x) {
-			push_up(x);
-			while (!root(x)) {
-				int y = f[x], z = f[y];
-				if (root(y) && son[y][0] == x)rt(x, 1);
-				else if (root(y) && son[y][1] == x)rt(x, 0);
-				else if (son[z][0] == y && son[y][0] == x)rt(y, 1), rt(x, 1);
-				else if (son[z][0] == y && son[y][1] == x)rt(x, 0), rt(x, 1);
-				else if (son[z][1] == y && son[y][0] == x)rt(x, 1), rt(x, 0);
-				else rt(y, 0), rt(x, 0);
+			top = 0; q[++top] = x;
+			for (int i = x; !isroot(i); i = fa[i])
+				q[++top] = fa[i];
+			for (int i = top; i; i--)
+				pushdown(q[i]);
+			while (!isroot(x))
+			{
+				int y = fa[x], z = fa[y];
+				if (!isroot(y))
+				{
+					if (c[y][0] == x ^ c[z][0] == y)rotate(x);
+					else rotate(y);
+				}
+				rotate(x);
 			}
 		}
 		void access(int x) {
-			for (int y = 0; x; x = f[x]) {
-				splay(x);
-				son[x][1] = y;
-				push_down(x);
-				y = x;
-			}
+			for (int t = 0; x; t = x, x = fa[x])
+				splay(x), c[x][1] = t;
 		}
-		void link(int x, int y) {
-			access(y); splay(y); f[x] = y;
-			size[y] += size[x]; tag[y] += size[x];
+		void link(int x, int f) {
+			fa[x] = f; access(f); splay(f); add(f, w[x]);
 		}
-		void cut(int x, int y) {
-			access(y); splay(x); f[x] = 0;
-			size[y] -= size[x]; tag[y] -= size[x];
+		void cut(int x) {
+			access(x); splay(x); add(c[x][0], -w[x]);
+			fa[c[x][0]] = 0; c[x][0] = 0;
 		}
 	}t;
 	struct SAM_node
 	{
 		SAM_node *pre, *next[26];
-		//int next2[26];//记录子节点位置，空间换时间，卡空间时可以去掉
-		int next2_sum;
 		int step;
-		long child;
-		long max_long;
-		long cur_long;
-		int _time;
-		int sum;//求字符串不同整数和
-		int cnt3;//求字符串不同整数和
 		int cnt;
-		unsigned long long ans;
+		int sign;
 		int firstpos;
-		void clear()
+		void clear(int cur)
 		{
-			ans = 0;
-			sum = 0;
+			sign = cur;
 			cnt = 0;
-			cnt3 = 0;
-			child = -1;
-			next2_sum = 0;
 			pre = 0;
 			step = 0;
-			cur_long = -1;
-			max_long = 101000;
-			_time = 0;
 			firstpos = 0;
 			memset(next, 0, sizeof(next));
 		}
@@ -113,7 +106,6 @@ namespace sam_lct
 	long d[N * 2];
 	SAM_node *pool[N * 2];
 	SAM_node SAM_Pool[N * 2];
-	int part2[26];
 	struct SAM //后缀自动机 Suffix_Automaton
 	{
 		SAM_node *root, *last;
@@ -123,9 +115,9 @@ namespace sam_lct
 		long length;
 		void init()
 		{
-			cur = SAM_Pool;  //cur=当前弧
+			cur = SAM_Pool;
 			root = last = cur++;
-			root->clear();
+			root->clear(cur - SAM_Pool);
 			length = 0;
 		}
 		void Insert(int w)
@@ -133,32 +125,40 @@ namespace sam_lct
 			length++;
 			SAM_node *p = last;
 			SAM_node *np = cur++;
-			np->clear();
+			np->clear(cur - SAM_Pool);
 			np->step = p->step + 1;
 			np->firstpos = np->step - 1; //确定原串初始位置
+			t.w[np->sign] = 1;
 			while (p && !p->next[w])
 			{
 				p->next[w] = np, p = p->pre;
 			}
 			if (p == 0)
+			{
 				np->pre = root;
+				t.link(np->sign, root->sign);
+			}
 			else
 			{
 				SAM_node *q = p->next[w];
 				if (p->step + 1 == q->step)
+				{
 					np->pre = q;
+					t.link(np->sign, q->sign);
+				}
 				else
 				{
 					SAM_node *nq = cur++;
-					nq->clear();
+					nq->clear(cur - SAM_Pool);
 					memcpy(nq->next, q->next, sizeof(q->next));
 					nq->firstpos = q->firstpos;
 					nq->cnt = q->cnt;
 					nq->step = p->step + 1;
 					nq->pre = q->pre;
-					t.link(nq - SAM_Pool, q->pre - SAM_Pool);
-					t.cut(q - SAM_Pool, q->pre - SAM_Pool);
-					t.link(q - SAM_Pool, nq - SAM_Pool);
+					t.link(nq->sign, q->pre->sign);
+					t.cut(q->sign);
+					t.link(q->sign, nq->sign);
+					t.link(np->sign, nq->sign);
 					q->pre = nq;
 					np->pre = nq;
 					while (p&&p->next[w] == q)
@@ -168,7 +168,7 @@ namespace sam_lct
 				}
 			}
 			last = np;
-			t.link(last-SAM_Pool, last->pre - SAM_Pool);
 		}
 	};
 }
+using namespace sam_lct;
---
+++ SAM/带del的SAM.cpp
@@ -0,0 +1,156 @@
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<deque>
+#include<iomanip>
+#include<sstream>
+#include<stack>
+#include<iostream>
+#include<limits>
+#include<bitset>
+#include<list>
+#include<queue>
+#include<memory>
+#include<functional>
+#include<unordered_map>
+using namespace std;
+namespace string_tool2
+{
+	const int N = 150000;
+	bool DEL[N * 2];
+	struct SAM_node
+	{
+		SAM_node *pre, *next[26];
+		int step, Del;
+		int firstpos, maxend;
+		bool *del, endd;
+		void clear(int n)
+		{
+			endd = false;
+			maxend = 0;
+			Del = n;
+			pre = 0;
+			step = 0;
+			firstpos = 0;
+			del = &DEL[n];
+			*del = false;
+			memset(next, 0, sizeof(next));
+		}
+	};
+	SAM_node SAM_Pool[N * 2];
+	SAM_node *last_pool[N * 2];
+	struct SAM //后缀自动机 Suffix_Automaton
+	{
+		SAM_node *root, *last;
+		SAM_node  *cur;
+		int length, Del;
+		void init()
+		{
+			cur = SAM_Pool;
+			root = last = cur++;
+			root->clear(0);
+			length = 0;
+			Del = 1;
+			last_pool[0] = root;
+		}
+		void Insert(int w)
+		{
+			SAM_node *p = last;
+
+			SAM_node *np = cur++;
+			Del++;
+			np->clear(Del);
+			np->step = p->step + 1;
+			np->firstpos = np->step;
+			while (p && (p->next[w] == NULL || *(p->next[w]->del) == true))
+			{
+				p->next[w] = np, p = p->pre;
+			}
+			if (p == 0)
+				np->pre = root;
+			else
+			{
+				SAM_node *q = p->next[w];
+				if (p->step + 1 == q->step)
+					np->pre = q;
+				else
+				{
+					SAM_node *nq = cur++;
+					nq->clear(q->Del);
+					memcpy(nq->next, q->next, sizeof(q->next));
+					//memcpy(nq->next2, q->next2, sizeof(q->next2));
+					nq->del = q->del;
+					nq->firstpos = q->firstpos;
+					nq->step = p->step + 1;
+					nq->pre = q->pre;
+					q->pre = nq;
+					np->pre = nq;
+					while (p && p->next[w] == q)
+					{
+						p->next[w] = nq, p = p->pre;
+					}
+				}
+			}
+			last = np;
+			last_pool[++length] = last;
+		}
+		void pop_back()
+		{
+			DEL[last->Del] = true;
+			--length;
+			last = last_pool[length];
+		}
+		void get_suf()
+		{
+			SAM_node *pp = last;
+			while (pp != root)
+			{
+				pp->maxend = length;
+				pp->endd = true;
+				pp = pp->pre;
+			}
+		}
+		void clear_suf()
+		{
+			SAM_node *pp = last;
+			while (pp != root)
+			{
+				pp->maxend = length;
+				pp->endd = false;
+				pp = pp->pre;
+			}
+		}
+		void print_min(int x)
+		{
+			get_suf();
+			SAM_node *p = root;
+			bool flag = 0;
+			int len = 0;
+			for (int i = 0; i < x; ++i)
+			{
+				len++;
+				for (int j = 0; j < 26; ++j)
+					if (p->next[j] != NULL && *p->next[j]->del == false)
+					{
+						p = p->next[j];
+						flag = 1;
+						break;
+					}
+				if (p->endd)
+					break;
+			}
+			if (p->endd && len != x)
+			{
+				cout << p->maxend - len + 1 << endl;
+			}
+			else
+			{
+				cout << p->firstpos - len + 1 << endl;
+			}
+			clear_suf();
+		}
+	};
+}
